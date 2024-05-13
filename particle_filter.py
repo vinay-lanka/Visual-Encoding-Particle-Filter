843d8443bee8f324c82325727e6e954e3ae6d3ce
Vinay Lanka <vinaylanka111@gmail.com>
2024-05-13 05:33:22 -0400

    add veckm approach

--- particle_filter.py
+++ particle_filter.py
@@ -7,13 +7,15 @@ from sensor_msgs.msg import Image
 from geometry_msgs.msg import TwistStamped
 from std_msgs.msg import Float64
 
-import os
 import numpy as np
 from numpy.random import uniform, randn
 import matplotlib.pyplot as plt
 import cv2
-from sklearn.cluster import KMeans
-import copy
+from scipy.stats import norm
+from filterpy.monte_carlo import systematic_resample
+import numexpr as ne
+import time
+
 
 #cv bridge to convert ros img to opencv img
 bridge = CvBridge()
@@ -31,9 +33,8 @@ plt.ion()
 fig, ax = plt.subplots()
 x, y = [],[]
 sc = ax.scatter(x,y)
-plt.xlim(-100,100)
-plt.ylim(-100,100)
-
+plt.xlim(-150,150)
+plt.ylim(-150,150)
 plt.draw()
 
 class ImageEncoder():
@@ -74,32 +75,53 @@ class ImageEncoder():
         sim = cv2.compareHist(hist1,hist2,2)
         return sim
 
+class Encoder:
+    def __init__(self):
+        self.d = 128
+        self.alpha = 0.1
+        self.W = np.stack([self.strict_standard_normal(self.d) for _ in range(2)], axis=0) * self.alpha
+        self.detector = cv2.SIFT_create(20)
+        
+    def strict_standard_normal(self, d):
+        y = np.linspace(0, 1, d+2)
+        x = norm.ppf(y)[1:-1]
+        np.random.shuffle(x)
+        return x
+    
+    def encode(self, img):
+        kp = self.detector.detect(img, None)
+        pts = cv2.KeyPoint_convert(kp)
+        # test = 1j * pts @ self.W
+        # Z = ne.evaluate('exp(test)')
+        # Z = [np.e**n for n in test]
+        Z = np.exp(1j * pts @ self.W).sum(axis=0)
+        Z = Z / np.linalg.norm(Z)
+        return Z
+
+    def similarity(self, x, y):
+        return np.absolute(np.sum(x * y.conj()))
+
 class ParticleFilter:
     def __init__(self, image_path, num_particles, initial_state, process_noise_std):
         self.num_particles = num_particles
-        #old gaussian sampling
-        # self.particles = np.array([initial_state + np.random.randn(2) * process_noise_std for _ in range(num_particles)])
-        #old uniform sampling
-        # x = np.random.randint(-200, 200, self.num_particles)
-        # y = np.random.randint(-200, 200, self.num_particles)
-        # self.particles = np.vstack((x, y)).T
-
+        #known init position
         self.particles = self.create_gaussian_particles(initial_state, process_noise_std, self.num_particles)
-
+        # self.particles = self.create_uniform_particles((-100,100), (-100,100), self.num_particles)
+        #plot
         x = self.particles[:, 0]
         y = self.particles[:, 1]
         sc.set_offsets(np.c_[x,y])
         fig.canvas.draw_idle()
-        plt.pause(0.1)
+        plt.pause(0.01)
 
         self.weights = np.ones(num_particles) / num_particles
-        self.encoder = ImageEncoder(image_path)
-        self.map_img = cv2.imread(image_path)
+        self.encoder = Encoder()
+        self.map_img = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2GRAY)
         self.img_sz = self.map_img.shape[0]
         self.map_sz = 1200
         self.resolution = self.img_sz/self.map_sz
 
-    def create_uniform_particles(self, x_range, y_range, hdg_range, N):
+    def create_uniform_particles(self, x_range, y_range, N):
         particles = np.empty((N, 2))
         particles[:, 0] = uniform(x_range[0], x_range[1], size=N)
         particles[:, 1] = uniform(y_range[0], y_range[1], size=N)
@@ -116,33 +138,36 @@ class ParticleFilter:
         velocity_arr = np.full((len(self.particles), 2), velocity)
         self.particles =  self.particles + velocity_arr
 
-    def measurement_model(self, img1, img2):
+    def measurement_model(self, encoding1, img2):
         # Compute image similarity using feature encodings
-        encoding1 = self.encoder.hist_encode(img1)
-        encoding2 = self.encoder.hist_encode(img2)
-        return self.encoder.hist_similarity_correlation(encoding1, encoding2)
+        encoding2 = self.encoder.encode(img2)
+        return self.encoder.similarity(encoding1, encoding2)
 
-    def resample_particles(self):
-        # Resample particles based on weights
-        indices = np.random.choice(np.arange(self.num_particles), size=self.num_particles, p=self.weights)
-        self.particles = np.array([self.particles[i] for i in indices])
-        self.weights = np.ones(self.num_particles) / self.num_particles
-    
+    def resample_from_index(self, particles, weights, indexes):
+        self.particles[:] = particles[indexes]
+        self.weights.resize(len(particles))
+        self.weights.fill(1.0 / len(weights))
+
     def update(self, img):
         # Update particle weights based on measurement
+        encoding1 = self.encoder.encode(img)
         for i in range(self.num_particles):
             # Generate a predicted image based on the current particle's state
             predicted_img = self.generate_predicted_image(self.particles[i], self.map_img)
+            
             if predicted_img is not None:
                 #similarity score is the difference in measurement (measurement - predicted measurement)
-                diff_measurement = self.measurement_model(predicted_img, img)
-                # self.weights[i] *= np.exp(-0.5 * (diff_measurement)**2) # Update the particle's weight based on the difference between predicted and actual measurements
-                self.weights[i] = diff_measurement
+                diff_measurement = self.measurement_model(encoding1, predicted_img)
+                self.weights[i] *= np.exp(-0.5 * (diff_measurement)**2) # Update the particle's weight based on the difference between predicted and actual measurements
+                # self.weights[i] = diff_measurement
             else:
                 return
-        print(self.weights)
+        # self.weights += 1.e-300 
+        # self.weights = (self.weights - min(self.weights))/ (max(self.weights)-min(self.weights))
+        # self.weights += 1.e-300 
         self.weights /= np.sum(self.weights) # Normalize the weights
-        self.resample_particles()
+        indexes = systematic_resample(self.weights)
+        self.resample_from_index(self.particles, self.weights, indexes)
 
     def state_to_pixel(self, state):
         predicted_x, predicted_y = state
@@ -166,8 +191,7 @@ class ParticleFilter:
         
     def get_estimate(self):
         # Estimate drone position by averaging particles weighted by their importance
-        # return np.average(self.particles, axis=0, weights=self.weights)
-        pos = self.particles[:, 0:1]
+        pos = self.particles[:, 0:2]
         mean = np.average(pos, weights=self.weights, axis=0)
         var  = np.average((pos - mean)**2, weights=self.weights, axis=0)
         return mean, var
@@ -183,7 +207,10 @@ def image_callback(msg):
     img_width = int(altitude * width_multiplier)
     img_height = int(altitude * height_multiplier)
     if img_width>0:
-        current_image = cv2.resize(drone_view, (img_width, img_height), interpolation = cv2.INTER_LINEAR)
+        current_image = cv2.cvtColor(cv2.resize(drone_view, 
+                                                (img_width, img_height), 
+                                                interpolation = cv2.INTER_LINEAR)
+                                    , cv2.COLOR_BGR2GRAY)
         # cv2.imwrite("test2.jpg", current_image)
 
 def altitude_callback(msg):
@@ -200,21 +227,21 @@ def main():
 
     image_path = "map_sq.png"
     num_particles = 200
-    initial_state = np.array([0, 0])  # Initial guess of drone position (pixel coordinates)
+    initial_state = np.array([49, 17])  # Initial guess of drone position (pixel coordinates)
     process_noise_std = np.array([10, 10]) 
     particle_filter = ParticleFilter(image_path, num_particles, initial_state, process_noise_std)
-
-    # # Main loop to update and predict
-    # for i in range(2):  # Simulate 20 frames
-    #     x = particle_filter.particles[:, 0]
-    #     y = particle_filter.particles[:, 1]
-    #     sc.set_offsets(np.c_[x,y])
-    #     fig.canvas.draw_idle()
-    #     plt.pause(0.1)
-    #     estimated_position, estimated_variance = particle_filter.get_estimate()
-    #     print("Estimated Drone Position:", estimated_position)
-    #     particle_filter.predict(velocity)
-    #     particle_filter.update(current_image)
+    time.sleep(0.2)
+    # Main loop to update and predict
+    for i in range(200):  # Simulate 20 frames
+        x = particle_filter.particles[:, 0]
+        y = particle_filter.particles[:, 1]
+        sc.set_offsets(np.c_[x,y])
+        fig.canvas.draw_idle()
+        plt.pause(0.01)
+        estimated_position, estimated_variance = particle_filter.get_estimate()
+        print("Estimated Drone Position:", estimated_position)
+        particle_filter.predict(velocity)
+        particle_filter.update(current_image)
     
     plt.waitforbuttonpress()
     # cv2.destroyAllWindows()
